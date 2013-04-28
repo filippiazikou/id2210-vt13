14b56d18fef3683ed13d789bf7e539c119dd7da4
filippiazikou <filippia.zikou@gmail.com>
2013-04-28 19:00:48 +0200

    Changed Interval time from 100 to 600

--- common/src/main/java/common/simulation/scenarios/Scenario1.java
+++ common/src/main/java/common/simulation/scenarios/Scenario1.java
@@ -9,13 +9,13 @@ public class Scenario1 extends Scenario {
         {
             StochasticProcess process1 = new StochasticProcess() {
                 {
-                    eventInterArrivalTime(constant(100));
+                    eventInterArrivalTime(constant(600));
                     raise(1, Operations.peerJoin(5), uniform(13));
                 }
             };
             StochasticProcess process2 = new StochasticProcess() {
                 {
-                    eventInterArrivalTime(constant(100));
+                    eventInterArrivalTime(constant(600));
                     raise(5, Operations.peerJoin(5), uniform(13));
                 }
             };
