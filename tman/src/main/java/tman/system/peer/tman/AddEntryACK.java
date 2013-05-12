fc80d363d2b24a40083c5c215737ad3994ff73d0
Dmitry Kazarin <kazarindn@gmail.com>
2013-05-12 09:40:10 +0200

    Right files on the right places

---
+++ tman/src/main/java/tman/system/peer/tman/AddEntryACK.java
@@ -0,0 +1,10 @@
+package tman.system.peer.tman;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kazarindn
+ * Date: 5/12/13
+ * Time: 9:35 AM
+ */
+public class AddEntryACK {
+}
---
+++ tman/src/main/java/tman/system/peer/tman/AddEntryRequest.java
@@ -0,0 +1,10 @@
+package tman.system.peer.tman;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kazarindn
+ * Date: 5/12/13
+ * Time: 9:39 AM
+ */
+public class AddEntryRequest {
+}
